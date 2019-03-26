e11cf4da545cf28a2ffca61c5131682a14150412
emmanuelnocks <emmanuel.mulea@canpango.com>
2019-03-26 20:46:41 +0200

    test 6

--- getprospects.php
+++ getprospects.php
@@ -21,7 +21,7 @@ $app->register(new Amqp\Silex\Provider\AmqpServiceProvider, [
         $connection = $app['amqp']['default'];
         $channel = $connection->channel();
 // $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
-$channel = $connection->channel();
+// $channel = $connection->channel();
 $channel->queue_declare('task_queue', false, true, false, false);
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 $callback = function ($msg) {
