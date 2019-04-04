34b11342a023e6bc844e9a84877e7f73065438e2
EmmanuelNocks <nocksmulea@gmail.com>
2019-04-04 11:18:59 +0200

    TimezoneV2

--- test2.php
+++ test2.php
@@ -41,25 +41,28 @@ try{
     // $channel->basic_publish($msg, '', 'task_queue');
     // echo " [x] Sent'\n";
     // $channel->close();
-    // $connection->close();
+    // $connection->close();zksin@ap.omron.com
     $T2 = date("Y-m-d h:i:s");
     $datetime2 =  explode(" ",$T2);
     $date2 = implode("",explode("-",$datetime2[0]));
     $time2 = implode("",explode(":",$datetime2[1]));
     $datetime2 =$datetime2[0]."T".$datetime2[1];
-    $dateData = explode(";","2019-04-04T02:09:28;2019-04-04T02:10:28");
+    $dateData = explode(";","2019-04-04T02:09:28;2019-04-04T02:56:28");
     $instance = new Thou();
+    
     $data = $instance->getProspects($dateData[0],$dateData[1]);
 
     if(count($data)>1){
         foreach ($data as $key => $value) {
             $instance->lookUpProspect($value->id, $value->email);
+            echo "Email ",$value->email, " \n";
         }
     }
     elseif(count($data)==1){
         $instance->lookUpProspect($data->id, $data->email);
     }
-
+    // $instance = new Thou();
+    // $instance->lookUpProspect('5244418', 'nagendra.krish@gmail.com');
 }
 catch(Exeption $e){
 
